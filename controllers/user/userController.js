0ca81af52127607090ecf23e28d294a93ae6410e
rishtansoft <rishtansoft@gmail.com>
2024-11-05 10:44:56 +0500

    feat: update api for owner profile

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -139,7 +139,7 @@ class UserControllers {
       const { user_id, phone_2, birthday, role } = req.body;
 
       console.log(req.body);
-      
+
 
       const user = await Users.findOne({
         where: {
@@ -199,9 +199,9 @@ class UserControllers {
         return next(ApiError.badRequest("Birthday was not entered"));
       }
 
-    if (phone_2 && !validateFun.validatePhoneNumber(phone_2)) {
-      return next(ApiError.badRequest("Phone number is not formatted correctly"));
-    }
+      if (phone_2 && !validateFun.validatePhoneNumber(phone_2)) {
+        return next(ApiError.badRequest("Phone number is not formatted correctly"));
+      }
 
       // Unik ID generatsiya qilish
       let uniqueId;
@@ -831,6 +831,35 @@ class UserControllers {
     } catch (error) {
       console.error('Error replacing avatar:', error);
       return res.status(500).json({ message: 'Error replacing avatar', error: error.message });
+    }
+  }
+
+  async updateOwnerProfile(req, res, next) {
+    const { user_id } = req.query;
+    const { lastname, firstname, phone_2, birthday, address } = req.body;
+
+    try {
+      const user = await Users.findByPk(user_id);
+
+      if (!user) {
+        return next(ApiError.notFound('Foydalanuvchi topilmadi'));
+      }
+
+      await user.update({
+        lastname,
+        firstname,
+        phone_2,
+        birthday,
+        address,
+      });
+
+      res.json({
+        success: true,
+        message: 'Foydalanuvchi muvaffaqiyatli yangilandi',
+        data: user,
+      });
+    } catch (error) {
+      next(ApiError.internal('Foydalanuvchini yangilashda xatolik yuz berdi'));
     }
   }
 
